35f0100ab860eb4a1b8a12f2fa3c9f2c57a25264
sky wing <wing@yahoo.com>
2015-10-25 22:03:59 +0900

    add ';'

--- common.js
+++ common.js
@@ -1,2 +1,2 @@
 var temp = document.getElementById( 'place' );
-temp.addEventListener( 'click',function(){ this.innerHTML="Clicked"},false); 
+temp.addEventListener( 'click',function(){ this.innerHTML="Clicked";},false); 
